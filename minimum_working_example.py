2e446002e4d08eefef23a778352041e7b50e7779
José Agustín Moreno Larios <agustin90m@gmail.com>
2021-08-03 13:50:03 -0400

    Started Minimum Working Example

---
+++ minimum_working_example.py
@@ -0,0 +1,23 @@
+"""
+Minimum working example to train a Lunar Lander Agent using a DQN algorithm
+
+Apollo Ammonites - NMA Deep Learning 2021
+Team members:
+    J. A. Moreno-Larios
+    ADD YOURSELVES HERE
+
+Requirements:
+    stable-baseline3
+    box2d-py
+    gym
+    pyvirtualdisplay
+    matplotlib
+    numpy
+    - Note that there will be additional dependencies. pip, anaconda or 
+        miniconda should be able to install them.
+    - For miniconda, be sure to execure 
+    'conda config --add channels conda-forge' to be able to 
+    install software on an isolated environment
+"""
+
+
